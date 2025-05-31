7eaa7c9d931b579f6acd3e7d5ff45af32956974f
Prabhav Yadav <prabhavy82@gmail.com>
2025-05-31 12:09:10 +0530

    no important change is made just small

--- index.java
+++ index.java
@@ -1,17 +1,10 @@
-filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
-# Generate new_filenames as a list containing the new filenames
-# using as many lines of code as your chosen method requires.
 
-
-new_filenames = []
-for filename in filenames:
-    if filename.endswith("hpp"):
-        filename.replace("hpp","hp")
-    else:
-        new_filenames.extend(filenames)
-        break
-        
-
-
-print(new_filenames)
-# Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"] 
+class prg 
+{ 
+    public static void main(String[] args) 
+{
+    int d = a > b ? a : b ; 
+    int e = c > d ? c : d ;
+    System.out.println("the greater number in this three "+ e ); 
+}
+}  
