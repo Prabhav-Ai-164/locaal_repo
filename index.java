fe2ecd4e86bdc13a547b8a43634aec71ae17e2d9
Prabhav Yadav <prabhavy82@gmail.com>
2025-05-31 12:19:30 +0530

    smal change

--- index.java
+++ index.java
@@ -1,8 +1,11 @@
+
 class prg 
 { 
-    void enter (int a , int b )
-    {
-    int c = a > b ? a : b ; 
-    System.out.println("the greater number is "+ c ); 
+    void enter (int a , int b , int c ,int d )
+{
+    int e = a > b ? a : b ; 
+    int f = c > e ? c : e ;
+    int g = d > f ? d : f ;
+    System.out.println("the greater number in this four number is  "+ g ); 
 }
-} 
+}  
