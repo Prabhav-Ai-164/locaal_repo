8733d70efd604e2dd30c3df452325b7e2e31725c
Prabhav Yadav <prabhavy82@gmail.com>
2025-05-31 12:25:50 +0530

    changing

--- index.java
+++ index.java
@@ -1,11 +1,10 @@
 
 class prg 
 { 
-    void enter (int a , int b , int c ,int d )
+    void enter (int a , int b , int c )
 {
     int e = a > b ? a : b ; 
     int f = c > e ? c : e ;
-    int g = d > f ? d : f ;
-    System.out.println("the greater number in this four number is  "+ g ); 
-}
+    System.out.println("the greater number in this four number is  "+ f ); 
+} 
 }  
