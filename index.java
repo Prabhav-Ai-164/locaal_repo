c098ba8d8f3ccf0a356fe4bafb5cd3ee94d9a4fc
Prabhav Yadav <prabhavy82@gmail.com>
2025-06-09 12:59:17 +0530

    applied the extension of the java

--- index.java
+++ index.java
@@ -2,8 +2,11 @@
 
 class prg 
 { 
-    void enter (int a , int b , int c )
-{
+       public static void main(String[] args)
+{ 
+    int a = 54 ;
+    int b = 23;
+    int c = 45 ; 
     int e = a > b ? a : b ; 
     int f = c > e ? c : e ;
     System.out.println("the greater number in this four number is  "+ f ); 
